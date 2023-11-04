8e68cbdab8ab476b0937fe5f4b71bc7f32e12683
gilang <gilangsukmaw07@gmail.com>
2023-11-04 19:35:21 +0700

    import repo, middleware and service on router

--- internal/api/router.go
+++ internal/api/router.go
@@ -4,6 +4,10 @@ import (
 	"digital_sekuriti_indonesia/bootstrap"
 	"digital_sekuriti_indonesia/config/yaml"
 	"digital_sekuriti_indonesia/internal/api/routes"
+	"digital_sekuriti_indonesia/internal/middlewares/jwt"
+	"digital_sekuriti_indonesia/internal/repositories"
+	"digital_sekuriti_indonesia/internal/service"
+	"digital_sekuriti_indonesia/migrations"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 )
@@ -19,11 +23,22 @@ func NewRouter(cfg *yaml.Config) *fiber.App {
 
 	fmt.Println("Database connection success!")
 
+	migrations.AutoMigration(db)
+
 	//repositories
+	var (
+		userRepo = repositories.NewUserRepo(db)
+	)
 
 	//middlewares
+	var (
+		authMidleware = jwt.NewAuthMiddleware(userRepo, cfg)
+	)
 
 	//service
+	var (
+		authService = service.NewAuthService(authMidleware, userRepo)
+	)
 
 	//group
 	api := router.Group("/api")
@@ -32,7 +47,9 @@ func NewRouter(cfg *yaml.Config) *fiber.App {
 		return c.Send([]byte("uwow"))
 	})
 
-	routes.HealthRouter(api)
+	routes.AuthRouter(api, authService)
+
+	//routes.HealthRouter(api)
 
 	return router
 }
