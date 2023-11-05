1a9a8a0d72bbefcf18f60ff47f3dcc059f1571ed
gilang <gilangsukmaw07@gmail.com>
2023-11-05 20:17:34 +0700

    change response to access_token

--- internal/api/dto/auth_dto.go
+++ internal/api/dto/auth_dto.go
@@ -4,16 +4,16 @@ import "digital_sekuriti_indonesia/internal/entities"
 
 // User is the presenter object which will be passed in the response by Handler
 type User struct {
-	ID    string `json:"id,omitempty"`
-	Email string `json:"email"`
-	Token string `json:"token"`
+	ID          string `json:"id,omitempty"`
+	Email       string `json:"email"`
+	AccessToken string `json:"access_token"`
 }
 
 func AuthSucces(data *entities.User, token string) User {
 	user := User{
-		ID:    data.ID,
-		Email: data.Email,
-		Token: token,
+		ID:          data.ID,
+		Email:       data.Email,
+		AccessToken: token,
 	}
 	return user
 }
