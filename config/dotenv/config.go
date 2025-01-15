8936136c2cc00495de6c118ff8d6b531c926864a
gilang <gilang.sukma@digitalsekuriti.id>
2025-01-15 09:57:27 +0700

    fix config

--- config/dotenv/config.go
+++ config/dotenv/config.go
@@ -1,25 +1,65 @@
 package dotenv
 
 import (
+	"errors"
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
+	"os"
+	"strconv"
 )
 
 type Config struct {
-	AppName       string `env:"APP_NAME"`
-	AppPort       int    `env:"APP_PORT"`
-	DBDialect     string `env:"DB_DIALECT"`
-	DBHost        string `env:"DB_HOST"`
-	DBPort        int    `env:"DB_PORT"`
-	DBName        string `env:"DB_NAME"`
-	DBUsername    string `env:"DB_USERNAME"`
-	DBPassword    string `env:"DB_PASSWORD"`
-	JWTKey        string `env:"JWT_KEY"`
-	JWTExpiredMin int    `env:"JWT_EXPIRED_MINUTE"`
-	LoggerEnable  bool   `env:"LOGGER_ENABLE"`
+	AppName        string `env:"APP_NAME"`
+	AppEnvironment string `env:"APP_ENVIRONMENT"`
+	AppPort        int    `env:"APP_PORT"`
+	DBDialect      string `env:"DB_DIALECT"`
+	DBHost         string `env:"DB_HOST"`
+	DBPort         int    `env:"DB_PORT"`
+	DBName         string `env:"DB_NAME"`
+	DBUsername     string `env:"DB_USERNAME"`
+	DBPassword     string `env:"DB_PASSWORD"`
+	JWTKey         string `env:"JWT_KEY"`
+	JWTExpiredMin  int    `env:"JWT_EXPIRED_MINUTE"`
+	LoggerEnable   bool   `env:"LOGGER_ENABLE"`
 }
 
 func NewLoadConfig() (*Config, error) {
+
+	envrionment := os.Getenv("APP_ENVIRONMENT")
+
+	if envrionment == "production" {
+
+		appPort, err := strconv.Atoi(os.Getenv("APP_PORT"))
+		if err != nil {
+			return nil, errors.New("error convert env app port")
+		}
+
+		dbPort, err := strconv.Atoi(os.Getenv("DB_PORT"))
+		if err != nil {
+			return nil, errors.New("error convert env db port")
+		}
+
+		jwtExpiredMinute, err := strconv.Atoi(os.Getenv("JWT_EXPIRED_MINUTE"))
+		if err != nil {
+			return nil, errors.New("error convert env db port")
+		}
+
+		cfg := &Config{
+			AppName:       os.Getenv("APP_NAME"),
+			AppPort:       appPort,
+			DBDialect:     os.Getenv("DB_DIALECT"),
+			DBHost:        os.Getenv("DB_HOST"),
+			DBPort:        dbPort,
+			DBName:        os.Getenv("DB_NAME"),
+			DBUsername:    os.Getenv("DB_USERNAME"),
+			DBPassword:    os.Getenv("DB_PASSWORD"),
+			JWTKey:        os.Getenv("JWT_EXPIRED_MINUTE"),
+			JWTExpiredMin: jwtExpiredMinute,
+			LoggerEnable:  false,
+		}
+
+		return cfg, nil
+	}
 
 	err := godotenv.Load()
 	if err != nil {
