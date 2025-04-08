f9aa4a55d4effc856d21e4c54e63a797c44e6084
gilang <gilang.sukma@digitalsekuriti.id>
2025-04-08 21:49:18 +0700

    Added payment month

--- internal/entities/payment.go
+++ internal/entities/payment.go
@@ -10,6 +10,7 @@ type PaymentHistory struct {
 	ID             string    `gorm:"type:varchar(36);primary_key;unique" json:"id"`
 	Total          int       `gorm:"type:integer" json:"total"`
 	PaymentReceipt string    `gorm:"type:varchar(255)" json:"payment_receipt"`
+	ForMonth       string    `gorm:"type:varchar(15)" json:"for_month"`
 	RoomId         string    `gorm:"type:varchar(36);not null" json:"room_id"`
 	CreatedAt      time.Time `gorm:"not null" json:"created_at," sql:"DEFAULT:CURRENT_TIMESTAMP"`
 	UpdatedAt      time.Time `gorm:"not null" json:"updated_at," sql:"DEFAULT:CURRENT_TIMESTAMP"`
